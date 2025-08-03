585e48bca32873d33e4527988f68a42ad5316a57
nehamahato <70688249+nehamahato@users.noreply.github.com>
2025-08-03 23:38:59 +0530

    Time: 1 ms (62.14%), Space: 46.8 MB (19.72%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -24,30 +24,46 @@
 //     }
 // }
 
+// class Solution {
+//     public int trap(int[] height) {
+//         int n = height.length;
+
+//         int leftMax[] = new int[n];
+//         int rightMax[] = new int[n];
+
+//         leftMax[0] = height[0];
+//         rightMax[n-1] = height[n-1];
+//         for(int i=1;i<n;i++){
+//             leftMax[i] = Math.max(leftMax[i-1],height[i]);
+//         }
+
+//         for(int i=n-2;i>=0;i--){
+//             rightMax[i] = Math.max(rightMax[i+1],height[i]);
+//         }
+
+//         int totalwater = 0;
+//         for(int i=0;i<n;i++){
+//             int water = Math.min(leftMax[i],rightMax[i]) - height[i];
+//             if(water>0){
+//                 totalwater += water;
+//             }
+//         }
+//         return totalwater;
+//     }
+// }
+
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
 
-        int leftMax[] = new int[n];
-        int rightMax[] = new int[n];
-
-        leftMax[0] = height[0];
-        rightMax[n-1] = height[n-1];
-        for(int i=1;i<n;i++){
-            leftMax[i] = Math.max(leftMax[i-1],height[i]);
+        while (left < right) {
+            leftMax = Math.max(leftMax,height[left]);
+            rightMax = Math.max(rightMax,height[right]);
+            totalWater += (leftMax<rightMax)?leftMax-height[left++]:rightMax-height[right--];
         }
-
-        for(int i=n-2;i>=0;i--){
-            rightMax[i] = Math.max(rightMax[i+1],height[i]);
-        }
-
-        int totalwater = 0;
-        for(int i=0;i<n;i++){
-            int water = Math.min(leftMax[i],rightMax[i]) - height[i];
-            if(water>0){
-                totalwater += water;
-            }
-        }
-        return totalwater;
+        return totalWater;
     }
 }
