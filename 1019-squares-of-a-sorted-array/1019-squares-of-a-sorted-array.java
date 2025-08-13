26dfea3ff8dd99d4aa1259ddef9bd275b8a0edc9
nehamahato <70688249+nehamahato@users.noreply.github.com>
2025-08-13 18:59:55 +0530

    Time: 1 ms (100%), Space: 47.6 MB (23.2%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,10 +1,36 @@
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+        
+//         for(int i=0;i<nums.length;i++){
+//             nums[i] = nums[i]*nums[i];
+//         }
+//         Arrays.sort(nums);
+//         return nums;
+//     }
+// }
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        
-        for(int i=0;i<nums.length;i++){
-            nums[i] = nums[i]*nums[i];
+        int i = 0;
+        int j = nums.length-1;
+        int res[] = new int[nums.length];
+        int index = nums.length-1;
+
+        while(i<=j){
+            
+            int leftSquare = nums[i]*nums[i];
+            int rightSquare = nums[j]*nums[j];
+
+            if(leftSquare>rightSquare){
+                res[index] = leftSquare;
+                i++;
+            }
+            else{
+                res[index] = rightSquare;
+                j--;
+            }
+            index--;
         }
-        Arrays.sort(nums);
-        return nums;
-    }
+        return res;
+    }  
 }
