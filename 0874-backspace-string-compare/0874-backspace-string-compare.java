9d64f28b711c72ade102b306781c863d9f0fea4c
nehamahato <70688249+nehamahato@users.noreply.github.com>
2025-08-23 23:41:42 +0530

    Time: 0 ms (100%), Space: 41.5 MB (92.11%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.java
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -1,29 +1,80 @@
+// class Solution {
+//     public boolean backspaceCompare(String s, String t) {
+
+//         Stack<Character> stackS = new Stack<>();
+//         Stack<Character> stackT = new Stack<>();
+
+//         for(int i=0;i<s.length();i++){
+//             char ch = s.charAt(i);
+//             if(ch == '#'){
+//                 if(!stackS.isEmpty()) stackS.pop();
+//             }
+//             else{
+//                 stackS.push(ch);
+//             }
+//         }
+
+//         for(int i=0;i<t.length();i++){
+//             char ch = t.charAt(i);
+//             if(ch == '#'){
+//                 if(!stackT.isEmpty()) stackT.pop();
+//             }
+//             else{
+//                 stackT.push(ch);
+//             }
+//         }
+
+//         return stackS.equals(stackT);   
+//     }
+// }
+
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
+     public boolean backspaceCompare(String s, String t) {
+        int i = s.length()-1;
+        int j = t.length()-1;
 
-        Stack<Character> stackS = new Stack<>();
-        Stack<Character> stackT = new Stack<>();
+        while(i>=0 || j>=0){
+            int skipS = 0;
+            int skipT = 0;
+            while(i>=0){
+                if(s.charAt(i)=='#'){
+                    skipS++;
+                    i--;
+                }
+                else if(skipS>0){
+                    skipS--;
+                    i--;
+                }
+                else{
+                    break;
+                }
+            }
 
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch == '#'){
-                if(!stackS.isEmpty()) stackS.pop();
+            while(j>=0){
+                if(t.charAt(j)=='#'){
+                    skipT++;
+                    j--;
+                }
+                else if(skipT>0){
+                    skipT--;
+                    j--;
+                }
+                else{
+                    break;
+                }
             }
-            else{
-                stackS.push(ch);
+
+            if(i>=0 && j>=0 &&s.charAt(i)!=t.charAt(j)){
+                return false;
             }
+
+            if((i>=0)!=(j>=0)){
+                return false;
+            }
+
+            i--;
+            j--;
         }
-
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if(ch == '#'){
-                if(!stackT.isEmpty()) stackT.pop();
-            }
-            else{
-                stackT.push(ch);
-            }
-        }
-
-        return stackS.equals(stackT);   
+        return true;
     }
-}
+}    
