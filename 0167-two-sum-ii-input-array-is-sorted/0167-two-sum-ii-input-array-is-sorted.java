e967f7b7645462e219290ef2876b4ae3202d1585
nehamahato <70688249+nehamahato@users.noreply.github.com>
2026-06-09 18:28:42 +0100

    Time: 2 ms (96.38%), Space: 48.7 MB (11.53%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,20 +1,35 @@
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int[] ans = new int[2];
+//         for(int i=0;i<numbers.length;i++){
+//             for(int j=i+1;j<numbers.length;j++){
+//                 if(numbers[i]+numbers[j]==target){
+//                     ans[i,j];
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] ans = new int[2];
-        int i=0;
-        int j=numbers.length-1;
+        int i = 0 ; 
+        int j = numbers.length-1;
 
         while(i<j){
-            if(numbers[i]+numbers[j]==target){
-                ans[0]=i+1;
-                ans[1]=j+1;
-                break;
+            if(numbers[i] + numbers[j] == target){
+                ans[0] = i+1;
+                ans[1] = j+1;
+                return ans;
             }
-            if(numbers[i]+numbers[j]<target){
-                i++;
+            else if(numbers[i] + numbers[j] > target){
+                j--;
             }
             else{
-                j--;
+                i++;
             }
         }
         return ans;
